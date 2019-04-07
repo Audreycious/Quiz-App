3d6a8d814a266a477d9b3e548181dc484f990a46
Audrey Foss <porcupine4@gmail.com>
2019-04-06 21:23:12 -0500

    solved all the punc errs, code near complete

--- quizApp.js
+++ quizApp.js
@@ -1,97 +1,128 @@
+
+const STORE = {info: {currentView: 'view0', questionNum: 0, currentCorrectAnswer: '', isCorrect: false, totalCorrect: 0, totalNumQuestions: 10 },
+    questions: [
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'},
+        {question: 'blah1', answer: [ 'blah1', 'blah2', 'blah3', 'blah4', 'the correct answer'], correct:'blah1'}]};
+
 // A function that listens for button clicks in the div#renderThis
-    // output = id of button clicked
+    // output = class of button clicked
     function handleBtnClicks() {
-        $('#renderThis').on( 'click', '#quizBtn', function(e) {
-            if (this.attr('class') === 'submit') {
-            STORE.find(id:'info').currentView = 'view1';
-        };
-    
-        else if (e.attr('id') === 'finalScoreBtn') {
-    
-        };
-    
-        if (e.attr('id') === 'finalScoreBtn') {
-    
-        };
+        $('#renderThis').on( 'click', '#quizBtn', function() {
+            // let correctAns = STORE.find( obj => obj.id === "info").currentCorrectAnswer;
+            decideView(btnClass, correctAns);
+            return true;
+    })};
+
+// A function that outputs if the answer is correct or not
+    function isCorrect() {
+        
     }
-
-// A function that on form submit checks for if the answer is correct
-    // then submits the view and any extra IDs to the decideView()
-
-// A function that checks if the answer is right
-    // 1st arg = answer selected, 2nd arg = correct answer
-    // output = true/false
-
 // A function that decides which view to change to
     // 1st arg = current view, 2nd arg = answer correct?
     // output = new view
+    function decideView(btnClass, correctAns) {
+        if (btnClass === 'startQ') {
+            STORE.id.currentView = 'view1';}
+
+        else if (btnClass === 'submit') {
+            STORE.id.currentView = 'view2';}
+
+        else if (btnClass === 'next') {
+            STORE.id.currentView = 'view3';}
+
+        else if (btnClass === 'startAgain') {
+            STORE.id.currentView = 'view1';};
+        };
+
+// A function that on form submit checks for if the answer is correct
+    // then submits the view ato the decideView()
+     
+
+// A function that checks if the answer is right
+    // 1st arg = answer selected, 2nd arg = correct answer
+    // output = true/false   
+    function checkAns(ansSelected, correctAns) {
+
+    };  
 
 // A function that checks the view num and updates the button text
     // Make an object with the possible texts
     // 
 
 // A function that renders the entire area of id: renderThis
-function render() {
-    $('#renderThis').html(STORE.find(function(obj) {
-        
-    }
-}
+function render(currentView) {
+    const currentView = STORE.find(function(obj) {
+
+    })};
+//     $('#renderThis').html(
+// }
 // Find the STORE entry with id = info, then return the value of that obj.currentView, then use that value to update the view to that view
 
 
-const STORE = [
-    { id: 'info', currentView: 'view0'},
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' },
-    { question: 'blah1', answer1: 'blah1', answer2:'blah2', answer3:'blah3', answer4:'blah4', correct:'blah3' }];
+
 
 // View 0 for starting Page
+const view0 = (`
 <div class="view0" role="LandingPage">
     <div id='welcomePicture'>
          <img src="https://longdonclub.co.uk/wp/wp-content/uploads/2017/12/Quiz@Kvarteret-e1518802228786.png"
            alt="An image welcoming you to the quiz!"></img>
-     </div>
- <button id="quizBtn" class='startQ'>Start Quiz</button>
+    </div>
+    <button id="quizBtn" class='startQ'>Start Quiz</button>
  </div>
+ `);
         
 //  View 1 for Question Page
-<div class="view1" role="QuestionPage">
-    <div class="questionPanel">
-    ${STORE[correct question number].question}
-    {/*  The question input from the correct object in the STORE goes here e.g. STORE[correct question number].question */}
-    </div>
-    <div class="answerPanel">
-    ${STORE[correct answer number].answer}
-    {/* The form with the answer inputs from the correct object in the STORE goes here e.g. STORE[correct question number].answer */}
+const view1 = (`
+<div id="view1" role="QuestionPage">
+    <div id="questionPanel">
+    ${STORE[questionNum].question}` // The question input from the correct object in the STORE goes here
+    +`</div>
+    <div id="answerPanel">`  // The answer array from the correct object in the STORE displays in the answer form here
+        +`<form action="" method="post"> 
+            <input type="radio" name="quizAns" id="1stRadioAnswer" value="1stRadioAnswer" /><label for="1stRadioAnswer">${STORE[questionNum].answer[1]}</label>
+            <input type="radio" name="quizAns" id="2ndRadioAnswer" value="2ndRadioAnswer"  /><label for="2ndRadioAnswer">${STORE[questionNum].answer[2]}</label>
+            <input type="radio" name="quizAns" id="3rdRadioAnswer" value="3rdRadioAnswer" /><label for="3rdRadioAnswer">${STORE[questionNum].answer[3]}</label>
+            <input type="radio" name="quizAns" id="4rdRadioAnswer" value="4rdRadioAnswer" /><label for="4rdRadioAnswer">${STORE[questionNum].answer[4]}</label><
+        </form>
     </div>
     <button id="quizBtn" class='submit'>Submit</button>
 </div>
+`);
 
-// View 3 for Response Page 
-<div class="view3 correct" role="ResponsePage">
-    <div class="responsePanel">
+// View 2 for Response Page 
+const view2 = (`
+<div id="view2" role="ResponsePage">
+    ${STORE.info.isCorrect ? `
+    <div id="responsePanel">Correct!</div>
+    <div id="correctAnsPanel">
+        <img src='https://www.google.com/search?q=thumbs+up&rlz=1C1CHBF_enUS842US842&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNue3K4rfhAhUFnKwKHeLsAkUQ_AUIDigB&biw=1707&bih=778&dpr=1.5#imgrc=RQajt-rDHysSWM:' alt='a thumbs up because you are right!'></img>`
+    :  
+    `
+    <div id="responsePanel">Wrong!</div>
+    <div id="correctAnsPanel">
+        <p>The correct answer is:</p>
+        <p id="correctAnsDisplay>${STORE.id.currentCorrectAnswer}</p>
+    `}
+    <div id='scoreDisplay'>
+        <p>Your current score is:</p>
+        <p id='currentScoreDisplay'>${STORE.id.totalCorrect} / ${STORE.id.questionNum}</p>
+    <button id="quizBtn" class='next'>Next</button>
+`);
 
-    </div>
-    <div class="correctAnsPanel">
-        {/* https://www.google.com/search?q=thumbs+up&rlz=1C1CHBF_enUS842US842&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNue3K4rfhAhUFnKwKHeLsAkUQ_AUIDigB&biw=1707&bih=778&dpr=1.5#imgrc=RQajt-rDHysSWM: */}
-            </div>
-    <button id="responseBtn">Next</button>
-        </div>
-
-// View 4 for Final Score Page 
-<div class="view3" role="ResponsePage">
-    <div class="responsePanel">
-
-    </div>
-    <div class="correctAnsPanel">
-        {/* https://www.google.com/search?q=thumbs+up&rlz=1C1CHBF_enUS842US842&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNue3K4rfhAhUFnKwKHeLsAkUQ_AUIDigB&biw=1707&bih=778&dpr=1.5#imgrc=RQajt-rDHysSWM: */}
-            </div>
-    <button id="finalScoreBtn">Start Again?</button>
-        </div>
+// View 3 for Final Score Page 
+const view3 = (`
+<div id="view3" role="FinalScorePage">
+    <p>Your FINAL score is:</p>
+    <p id='finalScoreDisplay'>${STORE.id.totalCorrect} / ${STORE.id.totalNumQuestions}</p>
+</div>
+<button id="quizBtn" class='startAgain'>Start again?</button>
+`);
