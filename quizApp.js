6e846642e46d3159de92882ad00be2fc17e60d22
Audrey Foss <porcupine4@gmail.com>
2019-04-08 21:58:24 -0500

    final touches on js, need work on css

--- quizApp.js
+++ quizApp.js
@@ -1,6 +1,6 @@
 
-const STORE = {
-    info: { currentView: 0, questionNum: -1, currentCorrectAns: '', isCorrect: false, totalCorrect: 0, totalNumQuestions: Number},
+let STORE = {
+    info: { currentView: Number, questionNum: Number, currentCorrectAns: '', isCorrect: false, totalCorrect: 0, totalNumQuestions: Number},
     
     questions: [
         { question: 'blah1', answer: ['blah1', 'blah2', 'blah3', 'blah4'], correct: 'blah1' },
@@ -20,36 +20,33 @@ const STORE = {
 // A function that listens for button clicks in the div#renderThis
 // event delegator
 function handleBtnClicks() {
+    let currentQuestionNum;
+    let questionObj;
     $('#renderThis').on('click', '#quizBtn', function () {
-        let currentQuestionNum = STORE.info.questionNum;
-        const questionObj = STORE.questions[currentQuestionNum];
-        iHaveAllTheAnswers(questionObj, currentQuestionNum);
+        if (STORE.info.currentView === 1) { // so it doesn't run except on the question page
+            currentQuestionNum = STORE.info.questionNum;
+            questionObj = STORE.questions[currentQuestionNum];
+            iHaveAllTheAnswers(questionObj);
+        }
         let btnClass = this.attr('class');
-        let newView = decideView(btnClass);
-        renderView(newView, currentQuestionNum);
-    })
-};
-
-// A function that returns the button class
-    // function findBtnClass(e) {
-    //     return this.attr('class');
-    // };
+        decideView(btnClass);
+        renderView();
+    });
+}
 
 // A function that checks which answer was selected
-//      then cascades into checkAns() to update the STORE then end 
-// output = updated STORE
-    function iHaveAllTheAnswers(questionObj, currentQuestionNum) {
-        if (STORE.info.currentView === 1) {
-            const ansSelected = $('input[name=quizAns]:checked label').text();
-            console.log(ansSelected);
-            const correctAns = questionObj.correct;
-            checkAns(ansSelected, correctAns);
-            handleQuestionNum(currentQuestionNum);
-        }
+//      then cascades into checkAns() to update the STORE, increases the questionNum, then end s
+// output = none, updated STORE
+    function iHaveAllTheAnswers(questionObj) {
+        let ansSelected = $('input[name=quizAns]:checked label').text();
+        console.log(ansSelected);
+        let correctAns = questionObj.correct;
+        checkAns(ansSelected, correctAns);
+        STORE.info.questionNum++; 
         return;
-    };
+    }
 
-// A function that checks if the answer is right, updates the store, returns true/false
+// A function that checks if the answer is right, updates the store, and adds to the total correct if it's true, then ends
 // 1st arg = answer selected 2nd arg = correct answer
 
     function checkAns(ansSelected, correctAns) {
@@ -58,7 +55,7 @@ function handleBtnClicks() {
             STORE.info.totalCorrect++;
         }
         return;
-    };
+    }
 
 // A function that decides which view to change to
 // 1st arg = current view, 2nd arg = answer correct?
@@ -81,43 +78,39 @@ function handleBtnClicks() {
 
         else if (btnClass === 'startAgain') {
             STORE.info.currentView = 1;
+            STORE.info.questionNum = 0;
+            STORE.info.totalCorrect = 0;
             return;
-        };
+        }
         throw console.error('THE VIEW IS MESSED UP');
-    };
+    }
 
 // A function that adds to the questionNum
 // output = new questionNum
-function handleQuestionNum(currentQuestionNum) {
-    console.log('`handleQuestionNum` ran');
-    if (currentQuestionNum === 9) {
-        currentQuestionNum === -1;
-        STORE.info.totalCorrect = 0;
-    }
-    currentQuestionNum++;
-    return;
-}; 
+// function handleQuestionNum() {
+//     console.log('`handleQuestionNum` ran');
+//     STORE.info.questionNum++;
+//     return;
+// }
 
 // A function which generates the view
-function generateView(newView, currentQuestionNum) {
-    if (currentQuestionNum >= 0) {
-        const currentQuestion = STORE.questions[(currentQuestionNum -1)].question;
-        const answerKey = STORE.questions[(currentQuestionNum -1)].answer;
-        const correctAns = STORE.questions[(currentQuestionNum -1)].correct;
-        const isCorrect = STORE.info.isCorrect;
-        const totalCorrect = STORE.info.totalCorrect;
-        const totalNumQuestions = STORE.info.totalNumQuestions;
-    }
-    // Should I change this into individual views with variable inputs?******
-    const view = [// View 0 for Landing Page
+function generateView(newView) {
+    let currentQuestionNum = (STORE.info.questionNum - 1); // because I updated the number already at this point
+    let currentQuestion = STORE.questions[currentQuestionNum].question;
+    let answerKey = STORE.questions[currentQuestionNum].answer;
+    let correctAns = STORE.questions[currentQuestionNum].correct;
+    let isCorrect = STORE.info.isCorrect;
+    let totalCorrect = STORE.info.totalCorrect;
+    let totalNumQuestions = STORE.info.totalNumQuestions;
+    let view = [// View 0 for Landing Page
         `
-        div id="view0" role="LandingPage">
+        <div id="view0" role="LandingPage">
             <div id='welcomePicture'>
-                 <img src="https://longdonclub.co.uk/wp/wp-content/uploads/2017/12/Quiz@Kvarteret-e1518802228786.png"
+                <img src="https://longdonclub.co.uk/wp/wp-content/uploads/2017/12/Quiz@Kvarteret-e1518802228786.png"
                    alt="An image welcoming you to the quiz!">
             </div>
             <button id="quizBtn" class='startQ'>Start Quiz</button>
-         </div>
+        </div>
          `,
 
         //  View 1 for Question Page
@@ -125,7 +118,7 @@ function generateView(newView, currentQuestionNum) {
         <div id="view1" role="QuestionPage">
             <div id="questionPanel">${currentQuestion}` // The question input from the correct object in the STORE goes here
             + `</div>
-            <div id="ansPanel">`  // The answer array from the correct object in the STORE displays in the answer form here
+            <div id="ansPanel">`  // The answer array from the correct object in the STORE displays in the answer form here 
             + `<form id='ansForm' action="" method="get"> 
                     <input type="radio" name="quizAns" id="1stRadioAns" /><label for="1stRadioAns">${answerKey[0]}</label>
                     <input type="radio" name="quizAns" id="2ndRadioAns" /><label for="2ndRadioAns">${answerKey[1]}</label>
@@ -153,7 +146,7 @@ function generateView(newView, currentQuestionNum) {
             `}
             <div id='scoreDisplay'>
                 <p>Your current score is:</p>
-                <p id='currentScoreDisplay'>${totalCorrect} / ${(currentQuestionNum -1)}</p>
+                <p id='currentScoreDisplay'>${totalCorrect} / ${(currentQuestionNum)}</p>
             <button id="quizBtn" class='next'>Next</button>
         `,
 
@@ -167,26 +160,24 @@ function generateView(newView, currentQuestionNum) {
         `];
     console.log('`generateView` ran');
     return view[newView];
-    };
+    }
 
 // A function that renders the entire area of #renderThis
-function renderView(newView, currentQuestionNum) {
+function renderView() {
     console.log('`renderView` ran');
-    const viewString = generateView(newView, currentQuestionNum);
+    let newView = STORE.info.currentView;
+    let viewString = generateView(newView);
     $('#renderThis').html(viewString);
-    };
+    }
 
-
-// A function that sets the number of questions in STORE.info
-    function setTotalNumQuestions() {
-        const totalQuestions = STORE.questions.length;
-        STORE.info.totalNumQuestions === totalQuestions;
+// A function that sets up the number of questions in STORE.info, also sets questionNum and currentView to 0 so it can load the landing page
+    function setUpQuiz() {
+        STORE.info.totalNumQuestions = STORE.questions.length;
+        STORE.info.questionNum = 0;
+        STORE.info.currentView = 0;
         return;
-    };
+    }
 
-// A function that 
-
-
-setTotalNumQuestions(); // Protects future expansion of the question block
-renderView(0, -1);
+setUpQuiz(); // Protects future expansion of the question block
+renderView(); // Renders the opening view on load too
 handleBtnClicks();
